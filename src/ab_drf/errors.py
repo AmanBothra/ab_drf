d1fd1df02abd032f7323642a5c1c11511d9cfe03
Aman Bothra <amanbothra1777@gmail.com>
2022-11-30 12:47:48 +0530

    update errors.pt

--- src/ab_drf/errors.py
+++ src/ab_drf/errors.py
@@ -1,34 +1,126 @@
-"""
-======
-Errors
-======
-This module holds exceptions for all other app
-"""
+from rest_framework.exceptions import ErrorDetail
+from rest_framework.views import exception_handler
+from rest_framework import status
 
-from rest_framework import exceptions, status
+# -----------------------------------------------------------------------------
 
 
-class ErrorMessage:
-    UNEXPECTED = "Unexpected error. We'll fix it soon!", -1
+def get_status(code):
+    """Get the human readable SNAKE_CASE version of a status code."""
+    for name, val in status.__dict__.items():
+        if not callable(val) and code is val:
+            return name.replace("HTTP_%s_" % code, "")
+    return "UNKNOWN"
 
 
-class APIException(exceptions.APIException):
+def modify_api_response(response):
     """
-    Exception class that caught by renderer and produce pretty output.
+    Modify API response format.
+    Example success:
+    {
+        "code": 200,
+        "status": "OK",
+        "data": {
+            "username": "username"
+        }
+    }
 
-    It also has ``error_code`` attribute that may be set by other app otherwise it'll be ``-1``
+    Example error:
+    {
+        "code": 404,
+        "status": "NOT_FOUND",
+        "errors": [
+            {
+                "title": "detail",
+                "detail": "Not found."
+            }
+        ]
+    }
     """
+    # # If errors we got this from the exception handler which already modified the response
+    # if status.is_client_error(response.status_code) or status.is_server_error(
+    #     response.status_code
+    # ):
+    #     return response
 
-    def __init__(self, detail=None, code=None, param=None):
-        if isinstance(param, dict):
-            detail = detail.format(**param)
+    # Modify the response data
+    modified_data = {}
+    modified_data["code"] = response.status_code
+    # modified_data["status"] = get_status(response.status_code)
 
-        super().__init__(detail=detail, code=code)
+    if response.data.get("errors"):
+        modified_data["status"] = "FAIL"
+        modified_data["message"] = response.data.get("errors")[0].get("detail")
+        modified_data["data"] = []
+        # modified_data["errors"] = response.data.get("errors")
+
+    else:
+        if response.data.get("status") is None:
+            modified_data["status"] = "OK"
+        else:
+            modified_data["status"] = response.data.get("status")
+        modified_data["message"] = response.data.get("message")
+        modified_data["data"] = response.data.get("data")
+
+    response.data = modified_data
+    return response
 
 
-class NotFound(APIException):
-    status_code = status.HTTP_404_NOT_FOUND
+def get_api_error(source, detail, code):
+    """
+    Return an error object for use in the errors key of the response.
+    http://jsonapi.org/examples/#error-objects-multiple-errors
+    """
+    error_obj = {}
+    error_obj["source"] = source
+    error_obj["detail"] = detail
+    if code:
+        error_obj["code"] = code
+    return error_obj
 
 
-class ValidationError(APIException):
-    status_code = status.HTTP_400_BAD_REQUEST
+def get_clean_errors(data):
+    """DRF will send errors through as data so let's rework it."""
+    errors = []
+    for k, v in data.items():
+        ed = ErrorDetail(v)
+        if isinstance(v, list):
+            v = ", ".join(v)
+        errors.append(get_api_error(source=k, detail=v, code=ed.code))
+    return errors
+
+
+def get_api_error_response(response):
+    """
+    Custom API error response format.
+    {
+        "code": 400,
+        "status": "BAD_REQUEST",
+        "errors": [
+            {
+                "source": "first_name",
+                "detail": "This field may not be blank."
+            }
+        ]
+    }
+    """
+    modified_data = {}
+    modified_data["code"] = response.status_code
+    modified_data["status"] = get_status(response.status_code)
+    modified_data["errors"] = get_clean_errors(response.data)
+    # modified_data["errors"] = response.data
+    response.data = modified_data
+    return response
+
+
+def custom_exception_handler(exc, context):
+    """Custom exception handler."""
+    # Call REST framework's default exception handler first,
+    # to get the standard error response.
+    response = exception_handler(exc, context)
+
+    # Create our custom response format
+    if response is not None:
+        response = get_api_error_response(response)
+
+    return response
--- src/ab_drf/serializers.py
+++ src/ab_drf/serializers.py
@@ -1,46 +1,30 @@
 from rest_framework import serializers
-from rest_framework.fields import empty
 
 
-class PassObject(object):
-    pass
+class CustomModelSerializer(serializers.ModelSerializer):
+    def __init__(self, *args, **kwargs):
+        fields = kwargs.pop("fields", None)
+        exclude = kwargs.pop("exclude", None)
+        read_only_fields = kwargs.pop("read_only_fields", None)
 
+        super(CustomModelSerializer, self).__init__(*args, **kwargs)
 
-class BaseSerializer(serializers.ModelSerializer):
-    pass_object = PassObject()
+        if fields is not None:
+            allowed = set(fields)
+            existing = set(self.fields)
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
 
-    def __init__(self, instance=None, data=empty, **kwargs):
-        self.instance = instance
-
-        if data is not empty:
-            self.initial_data = data
-
-        self.partial = kwargs.pop("partial", True)
-        self.pass_object = kwargs.pop("pass_object", self.pass_object)
-
-        kwargs.pop("many", None)
-
-        read_only_fields = getattr(self.Meta, "read_only_fields", None)
+        if exclude is not None:
+            existing = set(self.fields)
+            for field_name in existing:
+                if field_name in exclude:
+                    self.fields.pop(field_name)
 
         if read_only_fields is not None:
-            if not isinstance(read_only_fields, (list, tuple)):
-                raise TypeError(
-                    "The `read_only_fields` option must be a list or tuple. "
-                    "Got %s." % type(read_only_fields).__name__
-                )
-
-            if isinstance(read_only_fields, list):
-                read_only_fields = read_only_fields + [
-                    "created_at",
-                    "updated_at",
-                ]
-
-            if isinstance(read_only_fields, tuple):
-                read_only_fields = read_only_fields + (
-                    "created_at",
-                    "updated_at",
-                )
-
-            self.Meta.read_only_fields = read_only_fields
-
-        super(BaseSerializer, self).__init__(instance, data, **kwargs)
+            for f in read_only_fields:
+                try:
+                    self.fields[f].read_only = True
+                except KeyError:
+                    # not in fields anyway
+                    pass
